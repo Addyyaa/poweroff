37a1fa113837a69e1cbbc5f3dd4e97225b45f8d7
SHEN FENG <2698567570@qq.com>
2024-03-13 21:32:26 +0800

    修复 不同电脑ipconfig回显内容不一致导致ip检查出错问题 增加没有检索到设备的情况处理

--- tmp2.py
+++ tmp2.py
@@ -6,17 +6,33 @@ def lan_ip_detect():
     # 执行命令并获取输出
     result = subprocess.run(["ipconfig"], capture_output=True, text=True).stdout
     index = result.rfind("WLAN")
-    lan_content = result[index::]
+    lan_contents = result[index::].splitlines()
+    lan_contents.pop(0)
+    lan_contents.pop(0)
+    lan_content = []
+    for i in lan_contents:
+        if i != "":
+            lan_content.append(i)
+            lan_content.append("\n")
+        else:
+            break
+    lan_content = "".join(lan_content)
     ipv4_str = lan_content[lan_content.lower().find("IPv4".lower())::].splitlines()[0]
-    subnet_mask = lan_content[lan_content.lower().find("Mask".lower())::].splitlines()[0]
-    gate_way = lan_content[lan_content.lower().find("Gateway".lower())::].splitlines()[0]
-    gateway_ip = ip_match(gate_way)
-    subnet_mask = ip_match(subnet_mask)
+    mask_index = lan_content.lower().find("Mask".lower())
+    if mask_index == -1:
+        mask_index = lan_content.lower().find("子网掩码".lower())
+    subnet_mask_str = lan_content[mask_index::].splitlines()[0]
+    gateway_index = lan_content.lower().find("Gateway".lower())
+    if gateway_index == -1:
+        gateway_index = lan_content.lower().find("默认网关".lower())
+    gateway_str = lan_content[gateway_index::]
+    gateway_ip = ip_match(gateway_str)
+    subnet_mask = ip_match(subnet_mask_str)
     network = ipaddress.IPv4Network(f"{gateway_ip}/{subnet_mask}", strict=False)
     # 获取可用主机范围
     start_ip = list(network.hosts())[0]
     end_ip = list(network.hosts())[-1]
-    start_ip =str(start_ip)
+    start_ip = str(start_ip)
     end_ip = str(end_ip)
     return start_ip, end_ip
 
